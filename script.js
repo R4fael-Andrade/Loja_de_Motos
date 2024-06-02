55be183ed8433d7521c01e8308fcc4578cde9523
Rafael <rafael.sousadrd@gmail.com>
2024-06-02 16:29:23 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,30 @@
+$('document').ready(function () {
+    $('#telefone').mask('(00)00000-0000', {
+        placeholder: '(00) 00000-0000'
+    })
+
+    
+
+    // $('form').validate({
+    //     rules: {
+    //         nome: {
+    //             required: true
+    //         },
+    //         email: {
+    //             required: true
+    //         },
+    //         telefone: {
+    //             required: true
+    //         },
+    //         mensagem: {
+    //             required:true 
+    //         }
+    //     },
+    //     messages: {
+    //         nome: 'Digite seu nome',
+    //         email: 'Digite seu email',
+    //         telefone: 'Digite seu telefone',
+    //         mensagem: 'Digite sua mensagem'
+    //     }
+    // })
+})
